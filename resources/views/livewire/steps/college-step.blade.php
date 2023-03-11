6edc003a36170485b065c9b0bde999f91cb3a43d
Mr-Monty999 <montser.mohmed2022@gmail.com>
2023-03-11 14:45:40 +0200

    update wizard form ui

--- resources/views/livewire/steps/college-step.blade.php
+++ resources/views/livewire/steps/college-step.blade.php
@@ -6,20 +6,15 @@
         <button class="btn sidebar-btn mb-1">التخصص</button>
         <button class="btn sidebar-btn mb-1">نوع المساعدة</button>
     </div>
-    <br>
-    <br>
-    <div class="d-flex flex-column justify-content-center align-items-center">
-        <h1>الخطوة الثانية</h1>
-        <div class="col-6 mb-4">
-            <label for="exampleFormControlInput1" class="form-label">الكلية</label>
-            <select class="form-select" id="exampleFormControlInput1">
-                <option selected>الإقتصاد</option>
-                <option value="1">القانون</option>
-                <option value="2">التربية</option>
-                <option value="3">التجارة</option>
-            </select>
-        </div>
-        {{--
+    <div class="row">
+        <div class="container d-flex flex-column justify-content-center align-items-center">
+            <div style="margin-top:15%;" class="col-5">
+                <button class="selection-btn mb-1">الإقتصاد</button>
+                <button class="selection-btn mb-1">القانون</button>
+                <button class="selection-btn mb-1">التربية</button>
+                <button class="selection-btn mb-1">التجارة</button>
+            </div>
+            {{--
         <div class="col-6 mb-4">
             <label for="exampleFormControlInput1" class="form-label">التخصص</label>
             <select class="form-select" id="exampleFormControlInput1">
@@ -40,16 +35,16 @@
             </select>
         </div>
     </div> --}}
-        <nav class="navbar navbar-expand fixed-bottom py-3" id="mainNav">
-            <div class="container-fluid px-4 px-lg-5">
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul style="width: 100%;"
-                        class="navbar-nav ms-auto my-2 my-lg-0 justify-content-between justify-content-md-evenly">
-                        {{-- <li class="nav-item"><button class="btn head-btn" wire:click="nextStep">التالي</button></li> --}}
-                        <li class="nav-item"><button class="head-btn" wire:click="previousStep">السابق</button></li>
-                        <li class="nav-item"><a class="head-btn" href="{{ route('home') }}">الرئيسية</a></li>
-                    </ul>
+            <nav class="navbar navbar-expand fixed-bottom py-3" id="mainNav">
+                <div class="container-fluid px-4 px-lg-5">
+                    <div class="collapse navbar-collapse" id="navbarResponsive">
+                        <ul style="width: 100%;"
+                            class="navbar-nav ms-auto my-2 my-lg-0 justify-content-between justify-content-between">
+                            {{-- <li class="nav-item"><button class="btn head-btn" wire:click="nextStep">التالي</button></li> --}}
+                            <li class="nav-item"><button class="head-btn" wire:click="previousStep">السابق</button></li>
+                            <li class="nav-item"><a class="head-btn" href="{{ route('home') }}">الرئيسية</a></li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    </div>
+            </nav>
+        </div>
--- resources/views/livewire/steps/education-level-step.blade.php
+++ resources/views/livewire/steps/education-level-step.blade.php
@@ -6,48 +6,21 @@
         <button class="sidebar-btn mb-1">التخصص</button>
         <button class="sidebar-btn mb-1">نوع المساعدة</button>
     </div>
-    <br>
-    <br>
-    <div class="d-flex flex-column justify-content-center align-items-center">
-        <h1>الخطوة الأولى</h1>
-
-        <div class="col-6 mb-4">
-            <label for="exampleFormControlInput1" class="form-label">المرحلة</label>
-            <select class="form-select" id="exampleFormControlInput1">
-                <option selected>طالب بكلاريوس</option>
-                <option value="1">باحث ماجستير</option>
-                <option value="2">باحث دكتوراة</option>
-                <option value="3">بحوث ترقية</option>
-            </select>
+    <div class="row">
+        <div class="container d-flex flex-column justify-content-center align-items-center">
+            <div style="margin-top:15%;" class="col-5">
+                <button class="selection-btn mb-1">طالب بكلاريوس</button>
+                <button class="selection-btn mb-1">باحث ماجستير</button>
+                <button class="selection-btn mb-1">باحث دكتوراة</button>
+                <button class="selection-btn mb-1">بحوث ترقية</button>
+            </div>
         </div>
-
-        {{-- <div class="col-6 mb-4">
-            <label for="exampleFormControlInput1" class="form-label">الكلية</label>
-            <select class="form-select" id="exampleFormControlInput1">
-                <option selected>الإقتصاد</option>
-                <option value="1">القانون</option>
-                <option value="2">التربية</option>
-                <option value="3">التجارة</option>
-            </select>
-        </div>
-
-        <div class="col-6 mb-4">
-            <label for="exampleFormControlInput1" class="form-label">القسم</label>
-            <select class="form-select" id="exampleFormControlInput1">
-                <option selected>إقتصاد دولي</option>
-                <option value="1">إقتصاد إسلامي</option>
-                <option value="2">إقتصاد رياضي</option>
-                <option value="3">أخرى</option>
-            </select>
-        </div> --}}
-
-
     </div>
     <nav class="navbar navbar-expand fixed-bottom py-3" id="mainNav">
         <div class="container-fluid px-4 px-lg-5">
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul style="width: 100%;"
-                    class="navbar-nav ms-auto my-2 my-lg-0 justify-content-between justify-content-md-evenly">
+                    class="navbar-nav ms-auto my-2 my-lg-0 justify-content-between justify-content-between">
                     <li class="nav-item"><button class="head-btn" wire:click="nextStep">التالي</button></li>
                     <li class="nav-item"><a class="head-btn" href="{{ route('home') }}">الرئيسية</a></li>
                     {{-- <li class="nav-item"><button class="btn head-btn" wire:click="previousStep">السابق</button></li> --}}
